434ef58ac8f8935c4a9d2757c67ff58189657d59
Md Repon <repon7253@gmail.com>
2025-09-20 15:59:09 +0600

    add dark mode in navigation menu

--- app/page.js
+++ app/page.js
@@ -1,20 +1,47 @@
-'user client';
+'use client';
 import About from '@/app/components/About';
 import Header from '@/app/components/Header';
 import Navbar from '@/app/components/Navbar';
 import Services from '@/app/components/Services';
 import Work from '@/app/components/Work';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+  }, [isDarkMode]);
+
   return (
     <>
-      <Navbar />
-      <Header />
-      <About />
-      <Services />
-      <Work />
-      <Contact />
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Header isDarkMode={isDarkMode} />
+      <About isDarkMode={isDarkMode} />
+      <Services isDarkMode={isDarkMode} />
+      <Work isDarkMode={isDarkMode} />
+      <Contact isDarkMode={isDarkMode} />
+      <Footer isDarkMode={isDarkMode} />
     </>
   );
 }
